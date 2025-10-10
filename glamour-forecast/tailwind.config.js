c53655868dd30850dd3b9d873bb5870a30d85fb1
breyhanaariel <breyhanadickenson@gmail.com>
2025-10-10 08:11:50 -0400

    Create tailwind.config.js

---
+++ glamour-forecast/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#fff8fb',
+          100: '#ffeef6',
+          500: '#ff7fbf'
+        }
+      }
+    }
+  },
+  plugins: []
+}
