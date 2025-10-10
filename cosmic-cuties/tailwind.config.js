421c28600d9461cee783f02d99047f47816a70b6
breyhanaariel <breyhanadickenson@gmail.com>
2025-10-10 12:45:55 -0400

    Create tailwind.config.js

---
+++ cosmic-cuties/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cosmic: {
+          50: "#f8fbff",
+          100: "#eef6ff",
+          500: "#6f7cff"
+        }
+      }
+    }
+  },
+  plugins: []
+}
